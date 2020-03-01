8e5a37c2994ccf36ec709758d2d7a3c8d29abe19
Ankush Anshuman <ankush.anshuman@hpe.com>
2020-03-01 18:15:27 +0530

    1. added decoder for points to marker. 2. changed URI if navigation intent

--- app/src/main/java/com/example/testmaps/MapsActivity.java
+++ app/src/main/java/com/example/testmaps/MapsActivity.java
@@ -17,6 +17,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
@@ -74,7 +75,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
 //        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(origin).title("Home").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        mMap.addMarker(new MarkerOptions().position(destination).title("Kormangla").icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 //        mMap = googleMap;
 
